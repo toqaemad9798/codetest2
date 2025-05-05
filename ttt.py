eb609e5bc6994793a52772eba0ab4619d863613b
toqaemad9798 <tuqa.23csp17@student.uomosul.edu.iq>
2025-05-05 19:01:19 +0300

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -4,6 +4,6 @@ query = "SELECT * FROM users WHERE name = '" + username + "'"
 print("Running query:", query)
 # vulnerable_script.py
 
-user_input = input("Enter a math expression: ")
-result = eval(user_input)
+	year = eval(sys.argv[1])
+
 print(f"The result is: {result}")
